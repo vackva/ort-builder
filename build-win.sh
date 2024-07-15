cf819b0b9b864a07ace56de46212ea155321bc05
Valentin Ackva <74772523+vackva@users.noreply.github.com>
2024-07-15 10:59:18 +0200

    Update build-win.sh

--- build-win.sh
+++ build-win.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
-./build-win.bat model.required_operators_and_types.config MinSizeRel
-./build-win.bat model.required_operators_and_types.config Debug
+./build-win.bat MinSizeRel
+./build-win.bat Debug
